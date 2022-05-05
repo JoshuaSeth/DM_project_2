d173689d6512da33968f974be357262d52e35c14
JoshuaSeth <SB@accette.eu>
2022-05-05 10:54:36 +0200

    move finished stacking model to models

--- models/Stacking_model.py
+++ models/Stacking_model.py
@@ -1,3 +1,4 @@
+from json import load
 from data.load import load_data
 import os
 from datetime import datetime
@@ -16,31 +17,43 @@ import copy
 # Load data
 # df = load_data(add_day_parts=True, same_value_operations=[('site_id', 'price_usd', 'avg'), ('srch_id', 'price_usd', 'avg')], fts_operations=[('prop_starrating', 'visitor_hist_starrating', 'diff')], add_seasons=True)
 
-df = load_data(num_rows=10000)
+df = load_data()
+
+test = load_data(test=True)
+
 
 # Split into target and predictors
 y = df['booking_bool']
-X = df.drop('booking_bool', axis=1)
+X = df.drop(['booking_bool','click_bool', 'position', 'gross_bookings_usd'], axis=1)
 X = X.fillna(X.mean())
 
 # Split 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+# X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, y_train = X, y
+X_test = test
+X_test = X_test.fillna(X.mean()) #Mean of x or mean of x_test?
 
 # Normalize
+print('scaling')
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train.drop('date_time', axis=1))
 X_test = scaler.transform(X_test.drop('date_time', axis=1))
 
 # Set up stakcing regressor
-rf = StackingRegressor([('rf',RandomForestRegressor()), ('br',BayesianRidge()), ('ab',AdaBoostRegressor())])
+rf = StackingRegressor([('rf',RandomForestRegressor()), ('br',BayesianRidge()), ('ab',AdaBoostRegressor())], verbose=1)
 # rf=BayesianRidge()
 rf.fit(X_train, y_train)
 
-# Evaluate
+# Evaluate``
+print('testing')
 preds = rf.predict(X_test)
 
-[print(a,b) for a,b in zip(y_test, preds)]
+[print(a,b, c) for a,b, c in zip(test['srch_id'], test['prop_id'],preds)]
 
-mse = mean_squared_error(y_test, preds)
-mae = mean_absolute_error(y_test, preds)
-print('\nerrors\n\n', mse, mae)
+results = pd.DataFrame(zip(test['srch_id'], test['prop_id'],preds), columns=['srch_id', 'prop_id' 'y'])
+results = results.sort_values(['srch_id','y'],ascending=False).groupby('srch_id')
+print(results)
+
+# mse = mean_squared_error(y_test, preds)
+# mae = mean_absolute_error(y_test, preds)
+# print('\nerrors\n\n', mse, mae)
